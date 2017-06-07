5c86d11107f2f9e00cf090be4443d946c14b9e09
Jared Chapman <jared.chapman.0612@gmail.com>
2017-06-06 22:40:51 -0500

    First Real Commit

---
+++ test/specs/tests/trelloNegativeLogin.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+describe('The login page of Trello', function() {
+	it('Logs in if credentials are correct', function() {
+		browser.url('https://trello.com/');
+		browser.click('=Log In');
+		browser.waitForVisible('#user', 10000);					//This needs to be changed to page load
+		browser.setValue('#user', 'jared.chapman.0613@gmail.com');
+		browser.setValue('#password', 'amidoingthisright?');
+		browser.click('#login',0,0);
+		browser.waitForVisible('.member-initials', 10000);
+		var initials = 'JC';
+		var initialsShouldSay = browser.getText('.member-initials');
+		assert.equal(initialsShouldSay, initials);
+	});
+});
+
